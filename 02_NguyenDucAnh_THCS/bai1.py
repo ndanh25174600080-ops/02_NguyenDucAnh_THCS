bf185b4aaecbc91e16037e2fef999f3b54597429
ndanh25174600080-ops <ndanh.25174600080@sv.uneti.edu.vn>
2025-11-19 22:37:41 +0700

    02_NguyenDucAnh_THCS

--- 02_NguyenDucAnh_THCS/bai1.py
+++
@@ -1,8 +0,0 @@
-# Nhập vào một số
-i = int(input("Nhap i:"))
-# Dùng câu lệnh if...else để đưa ra điều kiện và in ra màn hình xem có phải là số chính phương hay không
-if i>0 :
-    print("i là số chính phương")
-else:
-    i<0
-    print("i không là số chính phương")
